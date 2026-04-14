3e891feb6d4fd2b92f890ee1b23f169791977a85
maheshkol <99maheshkolekar@gmail.com>
2026-04-14 13:42:56 +0530

    Update WanderlyHome.jsx

--- src/pages/WanderlyHome.jsx
+++ src/pages/WanderlyHome.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import API from "../config";
 
 export default function WanderlyHome() {
   const navigate = useNavigate();
