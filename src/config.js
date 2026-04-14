abfc9d1bba4e842a0a648f578c9c9563f811c267
maheshkol <99maheshkolekar@gmail.com>
2026-04-14 14:24:54 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const API = {
-  BASE: import.meta.env.VITE_API_URL || "https://wanderly-server.onrender.com"
+  BASE: import.meta.env.VITE_API_URL || "https://wanderly-server.onrender.com",
+  DESTINATIONS: "/destinations"
 };
 
 export default API;
